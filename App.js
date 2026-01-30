c1ecef9b84b097e45d7c1296d4445515b949faa7
SaiDurga-28 <saidurgaanisetti9@gmail.com>
2026-01-30 11:04:53 +0530

    First commit - ReactNative Basics

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import react from "react";
+import {View,Text,Button,TouchableOpacity,Image,StyleSheet} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MyImage from "./assets/favicon.png"
+function App(){
+    return(
+        <SafeAreaView>
+        {/* <View> */}
+             <Text style={{"color":"violet"}}>Hello</Text>
+              <Text>All</Text>
+               <Text>Good Morning</Text>
+               <TouchableOpacity>
+                <Text style={MyStyles.mytext}>I'm Sai Durga</Text>
+                </TouchableOpacity>
+                <Button title="Click Me"/>
+                <Image source={MyImage}/>
+        {/* </View> */}
+        </SafeAreaView>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+const MyStyles=StyleSheet.create({
+  mytext:{
+    color:"green",
+    fontSize:24,
+    fontWeight:900
+  }
+})
